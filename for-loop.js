10a144240e91bd1570e228a40e0411d2baa3f0dc
neenus <neenus@live.ca>
2018-06-17 02:22:09 +0000

    javascripting project completed js files

---
+++ for-loop.js
@@ -0,0 +1,8 @@
+var total = 0;
+var limit = 10;
+for (var i =0; i < limit; i++) {
+  // add one number to total after each iteration
+  total +=i;
+  //log total to terminal
+}
+ console.log(total);
